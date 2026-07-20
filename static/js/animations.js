2a360733f790cc844b1ac1e31b97c3594aedcae7
Hifzain <syedhifzain280@gmail.com>
2026-07-20 12:42:43 +0500

    Fix BOM encoding issue causing whitespace above navbar

--- static/js/animations.js
+++ static/js/animations.js
@@ -1,4 +1,4 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
     if (typeof AOS !== "undefined") {
         AOS.init({
             duration: 700,
